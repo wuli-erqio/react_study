8e2c77dfd1d0c7e897e28b39e4a76843de47ed9b
wuli-erqio <1766458200@qq.com>
2020-04-24 19:08:16 +0800

    筛选栏吸顶功能实现步骤2

--- day05/hkzf/src/components/Sticky/index.js
+++ day05/hkzf/src/components/Sticky/index.js
@@ -1,14 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
 import styles from './index.module.css'
 
 class Sticky extends Component {
+  // 创建ref对象
+  placeholder = createRef()
+  content = createRef()
+  handleScroll = () => {
+    const placeholderEl = this.placeholder.current
+    const contentEl = this.content.current
+
+    const { top } = placeholderEl.getBoundingClientRect()
+    if(top < 0) {
+      // 吸顶
+      contentEl.classList.add(styles.fixed)
+      placeholderEl.style.height= '40px'
+    } else {
+      // 不吸顶
+      contentEl.classList.remove(styles.fixed)
+      placeholderEl.style.height= '0px'
+    }
+  }
+  // 监听scroll事件
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+  // 解绑
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
   render() {
     return (
       <div>
         {/* 占位元素 */}
-        <div />
+        <div ref={this.placeholder}/>
         {/* 内容元素 */}
-        <div>{ this.props.children } </div>
+        <div ref={this.content}>{ this.props.children } </div>
       </div>
     )
   }
