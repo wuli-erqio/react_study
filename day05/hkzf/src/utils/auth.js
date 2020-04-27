3d43cb8d035c42e4f6f5a80ead3b2ee9ab37ef34
wuli-erqio <1766458200@qq.com>
2020-04-27 14:01:20 +0800

    我的页面-功能分析

---
+++ day05/hkzf/src/utils/auth.js
@@ -0,0 +1,15 @@
+const TOKEN_NAME = 'hkzf_token'
+
+// 获取token
+const getToken = () => localStorage.getItem(TOKEN_NAME)
+
+// 设置token
+const setToken = () => localStorage.setItem(TOKEN_NAME, value)
+
+// 清除token
+const removeToken = () => localStorage.removeItem(TOKEN_NAME)
+
+// 是否登录(有授权)
+const isAuth = () => !!getToken()
+
+export {getToken, setToken, removeToken, isAuth}
